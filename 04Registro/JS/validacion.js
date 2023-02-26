1af028f667b411ae931a3416a6b5e91337e0088d
joakinrg <jrojasg2100@alumno.ipn.mx>
2023-02-25 21:33:11 -0600

    formulariofecha

--- 04Registro/JS/validacion.js
+++ 04Registro/JS/validacion.js
@@ -12,6 +12,7 @@ LET es una variable de tipo "Protected"
 CONST -> valor constante
 */
 function validar(formulario){
+    //Nombre
     if(formulario.nombre.value.length <= 3){
         alert("Favor de ingresar más de 3 caracteres en el campo nombre");
         formulario.nombre.focus();
@@ -43,9 +44,9 @@ function validar(formulario){
         return false;
     }
 
-
+    //edad
     var edad=parseInt(formulario.edad.value);
-    alert(edad);
+    //alert(edad);
     if((edad <0) || (edad >=99)){
         alert("Ingresa una edad valida entre 01 y 99 años");
         formulario.edad.focus();
@@ -76,4 +77,34 @@ function validar(formulario){
         formulario.edad.focus();
         return false;
     }
+    //fecha nacimiento
+    let ingreso = document.getElementById("fecha").value;
+    let fechaIngresada = new Date(ingreso);
+
+    if (!!fechaIngresada.valueOf()) {
+        año = parseInt(fechaIngresada.getFullYear());
+        mes = parseInt(fechaIngresada.getMonth());
+        dia = parseInt(fechaIngresada.getDate());
+    }
+
+    let fechAct = new Date();
+    let annoAct = fechAct.getFullYear();
+
+    if (año > annoAct || año < "1923") {
+        alert("No se puede ingresar un año menor a 1923 ");
+        return false;
+    }
+    //Validacion de correo
+    //Obtener el campo de correo
+    // let email = formulario.correo.value;
+
+    //Crear una expresion regular
+    let prueba =
+    // /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    // /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    /([A-Za-z]+[0-9]+\.){10}\@([A-Za-z]+[0-9]){8}\.([A-Za-z]+[0-9]){3}/g;
+
+    alert("Email " +(prueba.test(email) ? " " : "no") + "valido");
+
+    return prueba.test();
 }
